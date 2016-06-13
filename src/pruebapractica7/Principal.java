06f0f5e10b92d8c2472bac32c8819566a780b6a7
joseluisclase <joseluis_993@hotmail.com>
2016-06-13 02:38:04 +0200

    Update and rename superclass.java to Principal.java

---
+++ src/pruebapractica7/Principal.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pruebapractica7;
+
+import java.io.EOFException;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
+/**
+ *
+ * @author alumno
+ */
+public class superclass /*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pruebapractica7;
+
+import java.io.EOFException;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
+/**
+ *
+ * @author alumno
+ */
+public class Principal extends PrincipalSuperclass {
+}
+
+
+
